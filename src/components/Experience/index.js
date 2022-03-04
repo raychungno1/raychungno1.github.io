f7babb70b6f979c0b4a95124db88ef4b3175f2e3
Ray Chung <raychungno1@gmail.com>
2022-03-04 09:59:15 -0500

    experience

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -28,7 +28,13 @@ function Experience({ experience }) {
           ))}
         </div>
       </div>
-      <div className="timeline__info"></div>
+      <div className="timeline__info">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
     </div>
   );
 }
